2c988677352ae88189e9607aef551e52af10c380
Anelia Savova <aneliqsavovich@abv.bg>
2019-02-21 08:38:02 +0200

    Demo site-HTML&CSS

---
+++ myscripts.js
@@ -0,0 +1,36 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 10000);
+}
+
+
+
+function myFunction() {
+    document.getElementsByClassName("topnav")[0].classList.toggle("responsive");
+}
+var acc = document.getElementsByClassName("button");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
